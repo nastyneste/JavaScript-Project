d1e1dc01863d3ebc042d9f6c178e47a1d2d9b9fc
nastyneste <jamesrneste@gmail.com>
2020-08-15 22:31:44 -0700

    Basic JavaScript Assignment
    Uploading first set of files/folders

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,20 @@
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br> \"Eat my shorts!\" Bart responded.");
+
+document.write(3 + 3);
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+	Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
+
+function My_First_Function() {		//Defining a function and naming it
+	var str = "This text is green!"; 	//Defining a variable and giving it a 
+										//string value
+	var result = str.fontcolor("green"); //Using the fontcolor method on
+										 //str variable
+	document.getElementById("Green_Text").innerHTML = result;	//Putting the value	
+										 //of result into HTML element with "Green_Text" id
+}
